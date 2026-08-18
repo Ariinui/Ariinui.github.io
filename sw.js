6d2b82ae510deb395e5e59b1c709bf3381275ae2
Ariinui <ariinuiirgina@gmail.com>
2026-08-17 16:33:37 -1000

    Regenere le site : Continuer la lecture affiche desormais le nom du livre

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const SHELL_CACHE = 'bam-shell-v1';
 const RUNTIME_CACHE = 'bam-runtime-v1';
-const SHELL_ASSETS = ["/", "/index.html", "/styles.css?v=4b67781a", "/script.js?v=40fd28f4", "/manifest.json", "/offline.html", "/icons/icon-192.png", "/icons/icon-512.png", "/icons/apple-touch-icon.png"];
+const SHELL_ASSETS = ["/", "/index.html", "/styles.css?v=4b67781a", "/script.js?v=0f06a7b0", "/manifest.json", "/offline.html", "/icons/icon-192.png", "/icons/icon-512.png", "/icons/apple-touch-icon.png"];
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
