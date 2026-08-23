aa70be7b87fea5b57b1238c63528faf366f261b0
Ariinui <ariinuiirgina@gmail.com>
2026-08-23 08:45:21 -1000

    Agrandit le bouton et le popover de filtre de signets sur mobile

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const SHELL_CACHE = 'bam-shell-v1';
 const RUNTIME_CACHE = 'bam-runtime-v1';
-const SHELL_ASSETS = ["/", "/index.html", "/styles.css?v=449e2cf1", "/script.js?v=f8e82fc6", "/manifest.json", "/offline.html", "/icons/icon-192.png", "/icons/icon-512.png", "/icons/apple-touch-icon.png"];
+const SHELL_ASSETS = ["/", "/index.html", "/styles.css?v=e3c136eb", "/script.js?v=f8e82fc6", "/manifest.json", "/offline.html", "/icons/icon-192.png", "/icons/icon-512.png", "/icons/apple-touch-icon.png"];
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
